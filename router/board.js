cd73238953573076e2577865163217f21094f0d1
oms801 <oms801@naver.com>
2021-07-22 16:11:23 +0900

    get_category_post order add

--- router/board.js
+++ router/board.js
@@ -19,8 +19,8 @@ router.post('/reply/:category', authChecker, (req, res) => {
 
 
 //카테고리 게시글 10개, 총 게시글 수 불러오기  - 유저, 행정사
-router.get('/post/all/:category/:offset', (req, res) => { 
-    bd.get_category_post(res, req.params.category, req.params.offset);   
+router.get('/post/all/:category/:offset/:order', (req, res) => { 
+    bd.get_category_post(res, req.params.category, req.params.offset, req.params.order);   
 });
 
 //클릭한 게시글 및 답변 불러오기 - 유저, 행정사
